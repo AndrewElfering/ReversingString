68969a076c538194bf0ce053d85d03f9d1b5995a
Drew <93015578+AndrewElfering@users.noreply.github.com>
2025-05-16 12:09:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-  	String myWord = "Applause";
+    String myWord = "Applause";
     String myWordReversed = "";
     
     for (int i = myWord.length() -1; i>=0; i--) {
