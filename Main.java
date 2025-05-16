9dddc46cea899e913cfdaa69a7a752b2a4921b46
Drew <93015578+AndrewElfering@users.noreply.github.com>
2025-05-16 12:09:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    String word = "Applause";
-    String wordReversed = "";
+  	String myWord = "Applause";
+    String myWordReversed = "";
     
-    for (int i = word.length() -1; i>= 0; i--) {
-    	wordReversed += word.charAt(i);
-    }
-    System.out.println(wordReversed);
+    for (int i = myWord.length() -1; i>=0; i--) {
+    	myWordReversed += myWord.charAt(i);
+        }
+    System.out.println(myWordReversed);
   }
 }
